6020e648a619c478bb9a1ac0f4c30124bfb030b0
Fabiana Masini <masinifabiana@gmail.com>
2022-08-16 17:10:24 -0300

    Home page

---
+++ app/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  
+  return (
+    <div style={{
+        backgroundColor: 'blue'
+    }}>
+      <p>Home</p>
+    </div>
+  )
+}
+
+export default Home
